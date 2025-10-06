6bde7d151089b3b900eaca03993efbf9c370ae5a
ari <40159649+aniknia@users.noreply.github.com>
2025-10-06 15:25:15 -0700

    add nix-shell for development on nixos

--- shell.nix
+++ shell.nix
@@ -4,4 +4,10 @@ pkgs.mkShell {
 		pkgs.gcc
 		pkgs.gnumake
 	];
+
+	shellHook = ''
+		echo "Starting C Shell Environment"
+		echo "GCC version $(gcc -dumpversion)"
+		echo "GNUMake version $(make --version | grep -oP 'GNU Make \K[0-9.]+')"
+	'';
 }
